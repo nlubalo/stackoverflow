0e78095d65e3959a0e0d51d41de9a39d83d8f5a3
nlubalo <nancyatienno@gmail.com>
2026-03-20 20:48:30 +0300

    filter out self instant

--- dbt/stack_dbt/models/facts/fct_funnel_segment.sql
+++ dbt/stack_dbt/models/facts/fct_funnel_segment.sql
@@ -2,12 +2,12 @@ WITH base AS(
     SELECT
         *,
         CASE
-          WHEN tags LIKE '%chatgpt%'
-          OR tags LIKE '%gpt%'
-          OR tags LIKE '%llm%'
-          OR tags LIKE '%language model%'
-          OR tags LIKE '%ai%'
-          OR tags LIKE '%artificial intelligence%'
+          WHEN tags ILIKE '%chatgpt%'
+          OR tags ILIKE '%gpt%'
+          OR tags ILIKE '%llm%'
+          OR tags ILIKE '%language model%'
+          OR tags ILIKE '%ai%'
+          OR tags ILIKE '%artificial intelligence%'
           THEN 'AI-related'
           ELSE 'Non-AI-related'
         END AS segment
@@ -19,10 +19,12 @@ SELECT segment,
     SUM(CASE WHEN answer_count >0 THEN 1 ELSE 0 END) AS answered_questions,
     SUM(has_accepted_answer) AS accepted_answer_questions,
     AVG(time_to_first_answer_seconds) AS avg_time_to_first_answer_seconds,
+    SUM(has_self_instant_answer) AS self_instant_answer_questions,
 
     --- Conversion rates
     SUM(CASE WHEN answer_count >0 THEN 1 ELSE 0 END) * 1.0 / NULLIF(COUNT(*), 0) AS answer_conversion_rate,
-    SUM(has_accepted_answer) * 1.0 / NULLIF(SUM(CASE WHEN answer_count >0 THEN 1 ELSE 0 END), 0) AS accepted_answer_conversion_rate
+    SUM(has_accepted_answer) * 1.0 / NULLIF(SUM(CASE WHEN answer_count >0 THEN 1 ELSE 0 END), 0) AS accepted_answer_conversion_rate,
+    SUM(has_self_instant_answer) * 1.0 / NULLIF(COUNT(*), 0) AS self_instant_answer_conversion_rate
 FROM base
 GROUP BY segment
 ORDER BY segment
--- dbt/stack_dbt/models/staging/question_answer_metric.sql
+++ dbt/stack_dbt/models/staging/question_answer_metric.sql
@@ -1,22 +1,51 @@
-WITH answer_count AS(
+WITH answers_enrinched AS(
+    SELECT
+        a.*,
+        a.owner_user_id,
+        CASE
+            WHEN a.owner_user_id = q.owner_user_id
+            AND a.creation_date >= q.creation_date THEN 1
+            ELSE 0
+        END AS is_self_instant_answer
+    FROM {{ source('stackoverflow_raw', 'answers') }} a
+    JOIN {{ source('stackoverflow_raw', 'questions') }} q
+    ON a.question_id = q.question_id
+),
+
+-- exclude for core metrics
+filtered_answers AS (
+    SELECT *
+    FROM answers_enrinched
+    WHERE is_self_instant_answer = 0
+),
+
+answer_count AS(
     SELECT
         question_id,
         COUNT(*) AS answer_count
-    FROM {{ source('stackoverflow_raw', 'answers') }}
+    FROM filtered_answers
     GROUP BY question_id
 ),
 accepted AS (
     SELECT
     question_id,
     MAX(CASE WHEN is_accepted THEN 1 ELSE 0 END) AS has_accepted_answer
-    FROM {{ source('stackoverflow_raw', 'answers') }}
+    FROM filtered_answers
     GROUP BY question_id
 ),
 first_answer AS(
     SELECT
         question_id,
         MIN(creation_date) AS first_answer_time
-    FROM {{ source('stackoverflow_raw', 'answers') }}
+    FROM filtered_answers
+    GROUP BY question_id
+),
+-- self-instant tracking
+self_instant_answers AS (
+    SELECT
+        question_id,
+        MAX(is_self_instant_answer) AS has_self_instant_answer
+    FROM answers_enrinched
     GROUP BY question_id
 )
 SELECT
@@ -24,11 +53,14 @@ SELECT
     q.creation_date as question_creation_date,
     q.tags,
     q.score as question_score,
+    q.view_count,
     COALESCE(ac.answer_count, 0) as answer_count,
     COALESCE(a.has_accepted_answer, 0) as has_accepted_answer,
+    COALESCE(self_instant_answers.has_self_instant_answer, 0) as has_self_instant_answer,
     fa.first_answer_time,
     datediff('second', q.creation_date, fa.first_answer_time) as time_to_first_answer_seconds
 FROM {{ source('stackoverflow_raw', 'questions') }} q
 LEFT JOIN answer_count ac ON q.question_id = ac.question_id
 LEFT JOIN accepted a ON q.question_id = a.question_id
 LEFT JOIN first_answer fa ON q.question_id = fa.question_id
+LEFT JOIN self_instant_answers ON q.question_id = self_instant_answers.question_id
